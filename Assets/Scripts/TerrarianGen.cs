6507cacb669eee6d9138c41770bf7aad7b52fde9
Nowa <135564143+Nowa771@users.noreply.github.com>
2024-04-22 13:17:42 +1000

    Added Generation of tree and leaves to the world

--- Assets/Scripts/TerrarianGen.cs
+++ Assets/Scripts/TerrarianGen.cs
@@ -4,21 +4,35 @@ using UnityEngine;
 
 public class TerrarianGen : MonoBehaviour
 {
-    public int dirtLayerHeight = 5;
 
+    [Header("Tile Sprites")]
     public Sprite grass;
     public Sprite dirt;
     public Sprite stone;
+    public Sprite log;
+    public Sprite leaf;
 
+    [Header("Trees")]
+    public int treeChance = 10;
+    public int minTreeHeight = 3;
+    public int maxTreeHeight = 6;
+
+    [Header("Generation Settings")]
     public bool generateCaves = true;   
+    public int dirtLayerHeight = 5;
     public float surfaceValue = 0.25f;
-    public int worldSize = 100;
-    public float caveFreq = 0.05f;
-    public float terrainFreq = 0.04f;
+    public int worldSize = 100;   
     public float heightMultiplier = 40f;
     public int heightAddition = 25;
+
+    [Header("Noise Settings")]
+    public float terrainFreq = 0.05f;
+    public float caveFreq = 0.05f;
     public float seed;
     public Texture2D noiseTexture;
+
+    private List<Vector2> worldTiles = new List<Vector2>();
+
 
     private void Start()
     {
@@ -47,9 +61,12 @@ public class TerrarianGen : MonoBehaviour
                 }
                 else
                 {
+                    // top layer of terrain
                     tileSprite = grass;
+
                 }
 
+                // Cave generation
                 if (generateCaves)
                 {
                     if (noiseTexture.GetPixel(x, y).r > surfaceValue)
@@ -62,13 +79,29 @@ public class TerrarianGen : MonoBehaviour
                     placeTile(tileSprite, x, y);
                 }
 
+                if (y >= height - 1)
+                {
+                    int t = Random.Range(0, treeChance);
+                    if (t == 1)
+                    {
+                        // Generate a tree
+                        if (worldTiles.Contains(new Vector2(x, y)))
+                        {
+                            GenerateTree(x, y + 1);
+                        }
+                        
+                    }
+                }
                
+
+
 
 
             }
         }
     }
 
+    // RNG of the world
     private void GenerateNoiseTexture()
     {
         noiseTexture = new Texture2D(worldSize, worldSize);
@@ -86,6 +119,32 @@ public class TerrarianGen : MonoBehaviour
     }
 
 
+    void GenerateTree(float x, float y)
+    {
+        // define tree
+
+        // Generate Logs
+        if (worldTiles.Contains(new Vector2(x, y - 1)))
+        {
+            int treeHeight = Random.Range(minTreeHeight, maxTreeHeight);
+            for (int i = 0; i < treeHeight; i++)
+            {
+                placeTile(log, x, y + i);
+            }
+
+            // Generate Leaves
+            placeTile(leaf, x, y + treeHeight);
+            placeTile(leaf, x, y + treeHeight + 1);
+            placeTile(leaf, x, y + treeHeight + 2);
+
+            placeTile(leaf, x - 1, y + treeHeight);
+            placeTile(leaf, x - 1, y + treeHeight + 1);
+
+            placeTile(leaf, x + 1, y + treeHeight);
+            placeTile(leaf, x + 1, y + treeHeight + 1);
+        }
+    }
+
     public void placeTile(Sprite tileSprite, float x, float y)
     {
         GameObject newTile = new GameObject();
@@ -94,5 +153,7 @@ public class TerrarianGen : MonoBehaviour
         newTile.GetComponent<SpriteRenderer>().sprite = tileSprite;
         newTile.name = tileSprite.name;
         newTile.transform.position = new Vector2(x + 0.5f, y + 0.5f);
+
+        worldTiles.Add(newTile.transform.position - (Vector3.one * 0.5f));
     }
 }
